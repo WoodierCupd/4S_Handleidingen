55d6e68169d366657461e093e5067d33e4943518
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2021-09-27 12:12:26 +0200

    finnished ticket 2b
    ticket about the footer and the content of the footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,11 @@
 
 <footer>
+  <h5>Over ons:</h5>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam commodi quam, dolor esse nesciunt harum earum consectetur? Aperiam nisi, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis aliquid ipsum accusamus voluptas veritatis qui, eos quos, tempora explicabo provident expedita earum et error libero, ab repellendus atque nihil! Quasi.</p>
+  <h5>Contact:</h5>
+  <p><b>Email:</b> Email@gmail.com <b>Phone:</b> 0612345678</p>
+  <h5>Social Links:</h5>
+  <p><b><a href="https://twitter.com/?lang=nl"><i class="fab fa-twitter-square"></i></a> <a href=""><i class="fab fa-facebook-square"></i></a> <a href=""><i class="fab fa-instagram"></i></a> <a href=""><i class="fab fa-discord"></i></a></b></p>
 	© {{ __('misc.copyright') }}
 </footer>
 
